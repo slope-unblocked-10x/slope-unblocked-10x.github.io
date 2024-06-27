83469e15a117f71215dc76e0c5f54ed3313a4c71
slope-unblocked-10x <kalalammxxxxx@yandex.com>
2024-06-27 22:48:25 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,53 @@
+import Randomizer from "../components/randomizer";
+import { useEffect, useState } from "react";
+import { data, DataTypes } from "../data";
+import Card from "../components/card";
 import { Link } from "react-router-dom";
-import Button from "../components/button";
+import Button from "../components/button"; 
 
-function Home() {
+function Games() {
+  const [results, setResults] = useState<DataTypes[]>(data);
+  const [query, setQuery] = useState<string>("");
+
+  useEffect(() => {
+    if (query.length > 0) {
+      const found = data.filter((game) => {
+        return query.toLowerCase() === ""
+          ? game
+          : game.title.toLowerCase().includes(query.toLowerCase());
+      });
+      setResults(found);
+    } else {
+      setResults(data);
+    }
+  }, [query]);
+
   return (
-    
     <>
-      <div>
-        <h2 className="text-2xl">Cosmic City</h2>
-        <p>The online city for students by students</p>
-      </div>
-      <Link to="/games">
+      <h2 className="text-2xl">Games</h2>
+      <div className="flex flex-col gap-2 justify-center items-center">  <Randomizer />
+        <input
+          type="text"
+          className="bg-[#111] rounded-xl text-sm outline-none px-6 py-2 my-2 transition duration-75 border-b-[1.75px] border-b-black focus:border-b-[#83c9e5]"
+          placeholder="Search games.."
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+          
+        />
+        <Link to="https://github.com/cosmic-city/cosmic-city.github.io/issues">
         <Button look="brand" className="mt-2">
-          Start playing!
+          Request a game
         </Button>
       </Link>
+      </div>
+      <div className="flex  justify-center">
+        <div className="flex flex-wrap max-w-[80vw] my-2 gap-4">
+          {results.map((game) => {
+            return <Card id={game.id} />;
+          })}
+        </div>
+      </div>
     </>
   );
 }
 
-export default Home;
+export default Games;
