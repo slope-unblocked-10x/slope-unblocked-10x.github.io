7327b0791f6462e8a6ceaacfe291530958db881b
slope-unblocked-10x <kalalammxxxxx@yandex.com>
2024-07-06 11:30:09 +0300

    s

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-  content: [".ads.{txt}", "./ads.txt"],
   theme: {
     extend: {
       backgroundSize: {
